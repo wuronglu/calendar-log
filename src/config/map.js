96d39658851299c724f8cc395a4e02fc2c345d4e
wuronglu <2923239110@qq.com>
2024-11-16 21:31:40 +0800

    使用 wps 链接进行地址映射

---
+++ src/config/map.js
@@ -0,0 +1,3 @@
+export default {
+	"2024-11-16": "https://kdocs.cn/l/cvxaPBebNtU2",
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,42 @@
 import React from "react";
 import dayjs from "dayjs";
 import CalendarComponent from "./CalendarComponent";
-import { useNavigate } from "react-router-dom";
+import urlMap from "../../config/map";
+import { message } from "antd";
 
+const title = "快乐生活每一天";
 const Home = () => {
 	const [selectDate, setSelectDate] = React.useState(dayjs());
-	const navigate = useNavigate();
+	const [messageApi, contextHolder] = message.useMessage();
 
 	const handleDateChange = (newDate) => {
 		setSelectDate(newDate);
 		const formattedDate = newDate.format("YYYY-MM-DD");
-		navigate(`/onedaylog?date=${formattedDate}`);
+		if (!urlMap[formattedDate]) {
+			messageApi.open({
+				type: "info",
+				content: "选中日期暂无日志",
+				duration: 1.5,
+			});
+			return;
+		}
+		window.location = urlMap[formattedDate];
+		messageApi.open({
+			type: "success",
+			content: "正在打开",
+			duration: 1.5,
+		});
 	};
 
 	return (
 		<div>
-			<div className="text-4xl text-center p-16">从现在开始种一棵树</div>
+			{contextHolder}
+			<div className="text-4xl text-center p-16">{title}</div>
 			<div className="flex justify-center items-center">
 				<CalendarComponent
 					selectDate={selectDate}
 					setSelectDate={handleDateChange}
 				/>
-			</div>
-			<div>
-				<p>选中的日期是: {selectDate.format("YYYY-MM-DD")}</p>
 			</div>
 		</div>
 	);
