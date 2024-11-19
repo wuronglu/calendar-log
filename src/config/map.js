0eceded99189752464351a40d377790d777a7b73
wuronglu <2923239110@qq.com>
2024-11-19 11:38:28 +0800

    add new log link

--- src/config/map.js
+++ src/config/map.js
@@ -2,4 +2,5 @@ export default {
 	"2024-11-16": "https://kdocs.cn/l/cvxaPBebNtU2",
 	"2024-11-17": "https://kdocs.cn/l/ck2PrhEaiIKO",
 	"2024-11-18": "https://kdocs.cn/l/cdGXN3Ni0Re4",
+	"2024-11-19": "https://kdocs.cn/l/cmifVzul07fJ",
 };
