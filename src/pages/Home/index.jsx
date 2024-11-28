e791cf88c40a65bf0b2d7879418745c55240ee13
wuronglu <2923239110@qq.com>
2024-11-28 12:11:30 +0800

    first commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,32 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import dayjs from "dayjs";
 import CalendarComponent from "./CalendarComponent";
 import { message, Flex, Spin, Space } from "antd";
-import fetchMapLinks from "../../api/getMapLinks";
+import mapLinks from "../../map-links/map-links";
 
 const Home = () => {
 	const [selectDate, setSelectDate] = React.useState(dayjs());
 	const [messageApi, contextHolder] = message.useMessage();
-	const [urlMap, setUrlMap] = React.useState({});
-
-	useEffect(() => {
-		fetchMapLinks()
-			.then((data) => {
-				if (data?.data) {
-					setUrlMap(data.data);
-				} else {
-					console.error("Invalid response from API");
-				}
-			})
-			.catch((error) => {
-				console.error("Failed to fetch map links:", error);
-			});
-	}, []);
 
 	const handleDateChange = (newDate) => {
 		setSelectDate(newDate);
 		const formattedDate = newDate.format("YYYY-MM-DD");
-		if (!urlMap[formattedDate]) {
+		if (!mapLinks[formattedDate]) {
 			messageApi.open({
 				type: "info",
 				content: "选中日期暂无日志，试试昨天的吧～",
@@ -34,7 +19,7 @@ const Home = () => {
 			});
 			return;
 		}
-		window.location = urlMap[formattedDate];
+		window.location = mapLinks[formattedDate];
 		messageApi.open({
 			type: "success",
 			content: "正在打开",
@@ -42,11 +27,11 @@ const Home = () => {
 		});
 	};
 
-	return Object.keys(urlMap).length > 0 ? (
+	return Object.keys(mapLinks).length > 0 ? (
 		<div>
 			{contextHolder}
 			<div className="text-2xl text-center p-16 break-words sm:text-4xl">
-				{`坚持不懈第 ${Object.keys(urlMap).length} 天`}
+				{`坚持不懈第 ${Object.keys(mapLinks).length} 天`}
 			</div>
 			<div className="px-4 flex justify-center items-center sm:px-0 ">
 				<CalendarComponent
